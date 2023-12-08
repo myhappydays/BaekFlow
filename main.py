fb009ed80898e562598addc5d9327a159bfc78a4
myhappydays <seokjun0515@gmail.com>
2023-12-08 18:46:18 +0900

    명령어 인자와 help를 작성했음.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="제곱해드림.", type=int)
+parser.add_argument("-n", "--new", help="make new file.", action="store_true")
+parser.add_argument("-s", "--submit", help="submit my file.", action="store_true")
+parser.add_argument("-g", "--git", help="add and commit, push my file.", action="store_true")
 args = parser.parse_args()
-print(args.square**2)
+if args.new:
+    print("make file")
+if args.submit:
+    print("submit file")
+if args.git:
+    print("git file")
