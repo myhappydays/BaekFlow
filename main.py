66b01b352661ce76c148099c62e22f8e3a3bb284
myhappydays <seokjun0515@gmail.com>
2023-12-09 21:05:13 +0900

    현재 열려있는 창 가져오기 구현

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import argparse
+import pygetwindow
+import json
+
+with open('setting.json') as f:
+    setting_data = json.load(f)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--new", help="make new file.", action="store_true")
@@ -7,7 +12,21 @@ parser.add_argument("-g", "--git", help="add and commit, push my file.", action=
 args = parser.parse_args()
 
 if args.new:
-    print("개발중입니다.")
+
+    active_window = pygetwindow.getAllTitles()
+
+    browser_title = []
+
+    for i in setting_data['browser']:
+        for j in active_window:
+            if j[len(j)-len(i):] == i:
+                browser_title.append(j)
+
+    print(browser_title)
+
+    browser_window = pygetwindow.getWindowsWithTitle(browser_title[0])[0]
+    browser_window.activate()
+    
 if args.submit:
     print("개발중입니다.")
 if args.git:
