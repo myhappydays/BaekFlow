bd0ec5aa98c6cca1226667092fe4ac5430100891
myhappydays <seokjun0515@gmail.com>
2023-12-09 18:13:22 +0900

    argparse로 실행 가능한 환경 만들었음

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--new", help="make new file.", action="store_true")
 parser.add_argument("-s", "--submit", help="submit my file.", action="store_true")
 parser.add_argument("-g", "--git", help="add and commit, push my file.", action="store_true")
 args = parser.parse_args()
+
 if args.new:
-    print("make file")
+    print("개발중입니다.")
 if args.submit:
-    print("submit file")
+    print("개발중입니다.")
 if args.git:
-    print("git file")
+    print("개발중입니다.")
