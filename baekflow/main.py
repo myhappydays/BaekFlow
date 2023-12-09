f0b6d712babd3c669784f11e87e14f4be3744558
myhappydays <seokjun0515@gmail.com>
2023-12-09 23:05:42 +0900

    파일 구조 변경

--- baekflow/main.py
+++ baekflow/main.py
@@ -3,6 +3,7 @@ import pygetwindow
 import json
 import pyautogui
 import clipboard
+import time
 
 with open('setting.json') as f:                                                                     #세팅파일 불러오기
     setting_data = json.load(f)
@@ -29,6 +30,8 @@ if args.new:
     browser_window = pygetwindow.getWindowsWithTitle(browser_title[0])[0]
     browser_window.activate()                                                                       #브라우저 창 포커스
 
+    time.sleep(0.1)
+
     pyautogui.hotkey('ctrl', 'l')
     pyautogui.hotkey('ctrl', 'c')                                                                   #url 복사
 
@@ -36,10 +39,27 @@ if args.new:
 
     site_url = []
     
-    for i in range(len(setting_data['site url'])):
+    for i in range(len(setting_data['site url'])):                                                  #특정 사이트 url 분류
         temp_url = setting_data['site url'][i]
         if temp_url in browser_url:
-            site_url.append(temp_url)
+            site_url.append(browser_url)
+
+    number = ""
+    for i in site_url[0][::-1]:                                                                     #문제 번호 추출
+        if i != '/':
+            number += i
+        else:
+            break
+    number = number[::-1]
+
+    file_name = ""
+
+    for i in setting_data['file name']:
+        if i == 'number':
+            file_name += number
+
+    print(setting_data['path']+file_name+setting_data['programing language'])                       #파일 만들기
+    f = open(setting_data['path']+file_name+setting_data['programing language'], 'w')
 
 if args.submit:
     print("개발중입니다.")
