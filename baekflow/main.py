e77837597b6bb34ac91244d6effeab742deb071b
myhappydays <seokjun0515@gmail.com>
2023-12-11 00:23:53 +0900

    코드 리팩토링

--- baekflow/main.py
+++ baekflow/main.py
@@ -7,147 +7,133 @@ import time
 import os
 import webbrowser
 
-with open('baekflow\setting\setting.json') as f:                                                                     #세팅파일 불러오기
-    setting_data = json.load(f)
+def load_settings():
+    # 설정 파일을 읽어서 반환
+    with open('baekflow\setting\setting.json') as f:
+        return json.load(f)
 
-browser_title = []
-site_url = []
-site_title = []
+def get_browser_title(setting_data):
+    # 현재 열려있는 창 중 브라우저 창을 식별하여 리스트로 반환
+    active_window = pygetwindow.getAllTitles()
+    browser_title = []
 
-def get_browser_title():
-    active_window = pygetwindow.getAllTitles()                                                      #현재 열려있는 창 불러오기
-
-    for i in setting_data['browser']:                                                               #브라우저 창 분류
+    for i in setting_data['browser']:
         for j in active_window:
-            if j[len(j)-len(i):] == i:
+            if j.endswith(i):
                 browser_title.append(j)
-    
-    print(browser_title)
 
-def get_browser_url():
-    for i in range(len(browser_title)):
-        temp_title = browser_title[i]
-        temp_windows = pygetwindow.getWindowsWithTitle(browser_title[i])[0]
+    return browser_title
+
+def get_browser_url(browser_title, setting_data):
+    # 각 브라우저 창에서 URL을 찾아 리스트로 반환
+    site_url = []
+    site_title = []
+
+    for title in browser_title:
+        temp_windows = pygetwindow.getWindowsWithTitle(title)[0]
         browser_window = temp_windows
-        print(temp_title)
-        browser_window.activate()                                                                       #브라우저 창 포커스
-
+        browser_window.activate()
         time.sleep(0.5)
 
         pyautogui.hotkey('ctrl', 'l')
-        pyautogui.hotkey('ctrl', 'c')                                                                   #url 복사
-
+        pyautogui.hotkey('ctrl', 'c')
         browser_url = clipboard.paste()
 
-        print("browser_url", browser_url)
-                
-        for i in range(len(setting_data['site url'])):                                                  #특정 사이트 url 분류
-            temp_url = setting_data['site url'][i]
+        for i, temp_url in enumerate(setting_data['site url']):
             if temp_url in browser_url:
                 site_url.append(browser_url)
-                site_title.append(temp_title)
+                site_title.append(title)
+
+    return site_url, site_title
+
+def create_file_name(number, name, setting_data):
+    file_name = ""
+
+    for j in setting_data['file name']:
+        if j == 'number':
+            file_name += number
+        elif j == 'name':
+            file_name += name
+        else:
+            file_name += j
+
+    return file_name
+
+def create_file(site_url, site_title, setting_data):
+    # 각 사이트에 대해 파일을 생성
+    for i, url in enumerate(site_url):
+        # 문제 번호 추출
+        number = url.split('/')[-1][::-1]
+        number = number[::-1]
+
+        # 문제 이름 추출
+        name = site_title[i].split("번: ")[1].split(" - Whale")[0]
+
+        # 설정된 파일 이름 생성
+        file_name = create_file_name(number, name, setting_data)
+
+        file_path = setting_data['path'] + file_name + setting_data['programing language']
+        ide_cmd = setting_data['ide']
+
+        print(file_path)
+        with open(file_path, 'w'):
+            pass
+
+        open_ide = os.popen(f'{ide_cmd} {file_path}').read()
+        print(open_ide)
+
+def submit_code(site_url, site_title, setting_data):
+    # 코드를 제출하는 함수
+    for i, url in enumerate(site_url):
+        # 문제 번호 추출
+        number = url.split('/')[-1][::-1]
+        number = number[::-1]
+
+        # 문제 이름 추출
+        name = site_title[i].split("번: ")[1].split(" - Whale")[0]
+
+        # 설정된 파일 이름 생성 
+        file_name = create_file_name(number, name, setting_data)
+
+        file_path = setting_data['path'] + file_name + setting_data['programing language']
+        with open(file_path, 'r') as f:
+            code = f.read()
+
+        clipboard.copy(code)
+
+        print(setting_data['submit url'] + number)
+        webbrowser.open(setting_data['submit url'] + number)
+
+        time.sleep(2)
+
+        pyautogui.hotkey('ctrl', 'v')
+        pyautogui.hotkey('ctrl', 'enter')
 
 def main():
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--new", help="make new file.", action="store_true")
-    parser.add_argument("-s", "--submit", help="submit my file.", action="store_true")
-    parser.add_argument("-g", "--git", help="add and commit, push my file.", action="store_true")
+    parser.add_argument("-n", "--new", help="Create a new file.", action="store_true")
+    parser.add_argument("-s", "--submit", help="Submit a file.", action="store_true")
+    parser.add_argument("-g", "--git", help="Add, commit, and push files.", action="store_true")
     args = parser.parse_args()
 
+    setting_data = load_settings()
+    browser_title = get_browser_title(setting_data)
+
     if args.new:
-        
-        get_browser_title()
-
-        if len(browser_title) != 0:
-
-            get_browser_url()
-
-            if len(site_url) != 0:
-                for i in range(len(site_url)):
-                    print("site_url", site_url[i])
-                    number = ""
-                    for j in site_url[i][::-1]:                                                                           #문제 번호 추출
-                        if j != '/':
-                            number += j
-                        else:
-                            break
-                    number = number[::-1]
-
-                    for j in range(len(site_title)):
-                        name = site_title[i].split("번: ")[1].split(" - Whale")[0]
-
-                    file_name = ""
-
-                    for j in setting_data['file name']:
-                        if j == 'number':
-                            file_name += number
-                        elif j == 'name':
-                            file_name += name
-                        else:
-                            file_name += j
-
-                    file_path = setting_data['path']+file_name+setting_data['programing language']
-                    ide_cmd = setting_data['ide']
-
-                    print(file_path)                                                                                #파일 만들기
-                    f = open(file_path, 'w')
-                    f.close()
-
-                    open_ide = os.popen(f'{ide_cmd} {file_path}').read()                                           #open ide
-                    print(open_ide)
-            
+        if browser_title:
+            site_url, site_title = get_browser_url(browser_title, setting_data)
+            if site_url:
+                create_file(site_url, site_title, setting_data)
             else:
                 print("Error: Not the specified page.")
-
         else:
             print("Error: Browser is not open.")
 
     if args.submit:
-        get_browser_title()
-
-        if len(browser_title) != 0:
-
-            get_browser_url()
-
-            if len(site_url) != 0:
-
-                for i in site_url:
-                    print("site_url", i)
-                    number = ""
-                    for i in i[::-1]:                                                                           #문제 번호 추출
-                        if i != '/':
-                            number += i
-                        else:
-                            break
-                    number = number[::-1]
-
-                    file_name = ""
-
-                    for i in setting_data['file name']:
-                        if i == 'number':
-                            file_name += number
-
-                    file_path = setting_data['path']+file_name+setting_data['programing language']
-                    f = open(file_path, 'r')
-
-                    code = ""
-
-                    while True:
-                        c = f.read()
-                        if c == '':
-                            break
-                        code += c
-
-                    clipboard.copy(code)
-
-                    print(setting_data['submit url'] + number)
-                    webbrowser.open(setting_data['submit url'] + number)
-
-                    time.sleep(2)
-
-                    pyautogui.hotkey('ctrl', 'v')
-                    pyautogui.hotkey('ctrl', 'enter')
+        if browser_title:
+            site_url, site_title = get_browser_url(browser_title, setting_data)
+            if site_url:
+                submit_code(site_url, site_title, setting_data)
 
     if args.git:
-        print("개발중입니다.")
+        print("개발 중입니다.")
