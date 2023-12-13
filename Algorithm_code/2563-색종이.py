2bb7a410410a67851dfbfc8acd4f1f23af37cafe
myhappydays <seokjun0515@gmail.com>
2023-12-13 19:43:59 +0900

    test

--- Algorithm_code/2563-색종이.py
+++ Algorithm_code/2563-색종이.py
@@ -1,10 +1,1 @@
-Paper = [[0 for _ in range(101)] for _ in range(101)]
-I = int(input())
-Color_Paper = [list(map(int, input().split())) for _ in range(I)]
-
-for i in Color_Paper:
-    for a in range(i[0], i[0] + 10):
-        for b in range(i[1], i[1] + 10):
-            Paper[a][b] = 1
-
-print(sum([Paper[i].count(1) for i in range(101)]))
+print("hello")
